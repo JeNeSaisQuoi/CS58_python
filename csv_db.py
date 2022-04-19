c03041185b851e10da936980fefebbd38df28fd9
John J Clawson <jbvibrations@gmail.com>
2022-04-19 10:05:46 -0700

    basic functions fleshed out
    -All the basic functions are defined
    -renamed .csv file

--- csv_db.py
+++ csv_db.py
@@ -1,7 +1,28 @@
 import csv
 
-with open('name_db.csv', 'r') as csv_file:
+with open("local_db.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
 
     for line in csv_reader:
         print(line)
+
+
+def insert_record():
+    # this is a comment
+    print("inserting record")
+
+
+def find_record():
+    print("finding record")
+
+
+def update_record():
+    print("updating record")
+
+
+def delete_record():
+    print("deleting record")
+
+
+def sell():
+    print("selling item")
